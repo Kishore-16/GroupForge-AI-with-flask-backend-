be9d28febd03c7cdee33917afdc73e9a58653511
Deepesh <deepeshcdm@gmail.com>
2026-02-23 15:25:32 +0530

    Things got deleted

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from 'lovable-tagger'
 export default defineConfig(({ mode }) => ({
     server: {
         host: "::",
-        port: 8080,
+        port: 54571,
         proxy: {
             '/api': {
                 target: 'http://localhost:5000',
